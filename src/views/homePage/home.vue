9937026091b17d9d8aeb8a4c9fe2b88cc666c8d4
Wifz Cybershell <wifzg0d@gmail.com>
2021-03-13 01:30:46 -0300

    fix(home): Busca Responsiva

--- src/views/homePage/home.vue
+++ src/views/homePage/home.vue
@@ -1,7 +1,36 @@
 <template>
-  <v-row>
-      <v-col cols="12">
-          <p>Hello!!!</p>
+  <v-row justify="center" align="center" no-gutters class="homepage">
+      <v-col 
+        cols="12" 
+        sm="10" 
+        md="8" 
+        lg="4" 
+        xl="2"
+      >
+        <v-row justify="center">
+            <v-col cols="12">
+              <v-img 
+                src="@/assets/logo.svg" 
+                contain 
+                max-height="100"
+              />
+            </v-col>
+            <v-col md="10">
+              <v-text-field
+                class="inputCustom"
+                solo
+                hide-details
+                placeholder="Copacabana"
+                style="font-size: 1.3rem !important;"
+              >
+                <template v-slot:prepend-inner>
+                    <v-icon size="35" class="mr-2 ml-2">
+                        mdi-magnify
+                    </v-icon>
+                </template>
+              </v-text-field>
+            </v-col>
+        </v-row>
       </v-col>
   </v-row>
 </template>
@@ -12,6 +41,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="sass">
+.homepage
+ .inputCustom > 
+  .v-input__control > 
+   .v-input__slot 
+    height: 67px !important
 </style>
