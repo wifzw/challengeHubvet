9c65faa398609c1611d64ed10cb442d0a73fe3f2
Wifz Cybershell <wifzg0d@gmail.com>
2021-03-13 00:30:19 -0300

    fix(router): Configurando homepage feat(homepage): Tela criada e pronta para o desenvolvimento

---
+++ src/views/homePage/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+      <v-col cols="12">
+          <p>Hello!!!</p>
+      </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
