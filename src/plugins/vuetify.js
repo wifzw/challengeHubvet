ca1787f8cc064e9d1c1620d509c84f7ee2213e00
Wifz Cybershell <wifzg0d@gmail.com>
2021-03-13 03:28:20 -0300

    feat(home): Implementações

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,8 +11,8 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: "#ee44aa",
-        secondary: "#424242",
+        primary: '#44BBA4',
+        secondary: '#757575',
         accent: "#82B1FF",
         error: "#FF5252",
         info: "#2196F3",
--- src/views/homePage/home.vue
+++ src/views/homePage/home.vue
@@ -4,8 +4,7 @@
         cols="12" 
         sm="10" 
         md="8" 
-        lg="4" 
-        xl="2"
+        xl="4"
       >
         <v-row justify="center">
             <v-col cols="12">
@@ -15,7 +14,7 @@
                 max-height="100"
               />
             </v-col>
-            <v-col md="10">
+            <v-col md="6">
               <v-text-field
                 class="inputCustom"
                 solo
@@ -30,6 +29,89 @@
                 </template>
               </v-text-field>
             </v-col>
+            <v-col cols="12" class="mt-2">
+             <v-row>
+                <v-col class="flex-shrink-1 flex-grow-1 mr-2">
+                 <v-select
+                   v-model="modelPackets"
+                   :items="itemsDemo"
+                   label="Buscar por"
+                 />
+                </v-col>   
+                 <v-col class="flex-shrink-1 flex-grow-1 mr-2">
+                  <v-select
+                   v-model="modelCity"
+                   :items="itemsCity"
+                   label="Onde você está?"
+                  />
+                </v-col>
+                <v-col class="flex-shrink-1 flex-grow-1 d-flex align-center">
+                    <v-row no-gutters>
+                        <v-col cols="12" class="d-flex justify-center ml-md-5">
+                         <v-btn 
+                           v-model="milesAndBrl.modelMiles"
+                           outlined 
+                           :color="milesAndBrl.modelBrl === true ? 'secondary' : 'primary'"
+                           @click="changeMilles(milesAndBrl)"
+                          >                    
+                          <v-icon 
+                            :color="milesAndBrl.modelBrl === true ? 'secondary' : 'primary'"
+                          >
+                             mdi-circle-multiple-outline
+                          </v-icon>
+                          </v-btn>
+                          <v-btn 
+                            v-model="milesAndBrl.modelBrl"
+                            outlined 
+                            :color="milesAndBrl.modelBrl === true ? 'primary' : 'secondary'"
+                            @click="changeMilles(milesAndBrl)"
+                          >
+                            <v-icon  :color="milesAndBrl.modelBrl === true ? 'primary' : 'secondary'">
+                                mdi-currency-usd
+                            </v-icon>
+                          </v-btn>    
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-col md="5" class="ml-4 ">
+                <!-- :search-input.sync="search" -->
+                 <v-combobox
+                   v-model="model"
+                   :items="itemsDemo"
+                   hide-selected
+                   label="Etiquetas"
+                   multiple
+                   persistent-hint
+                   small-chips
+                 >
+                 <template v-slot:selection="data">
+                <v-chip
+                  color="error"
+                  outlined
+                  v-bind="data.attrs"
+                  :input-value="data.selected"
+                  @click:close="data.parent.selectItem(data.item)"
+                >
+                 {{ data.item }}
+                </v-chip>
+                 </template>
+               </v-combobox>
+              </v-col>   
+             </v-row>
+            </v-col>
+            <v-col 
+              cols="12" 
+              class="d-flex justify-center"
+             >
+              <v-btn
+                color="primary"
+                tile
+                width="185"
+                height="38"
+              >
+                Pesquisar
+              </v-btn>
+            </v-col>
         </v-row>
       </v-col>
   </v-row>
@@ -38,6 +120,37 @@
 <script>
 export default {
 
+ data: () => ({
+    itemsDemo: [
+     'Promoção', 
+     'Novos', 
+     'Últimos dias'
+    ],
+    modelPackets: null,
+    modelCity: null,
+    modelMilhas: null,
+    itemsCity: [
+        'São Paulo',
+        'Lages',
+        'Blumenau'
+    ],
+    milesAndBrl: {
+        modelBrl: false,
+        modelMiles: true
+    },
+    model: null
+ }),
+ methods: {
+     changeMilles(item) {
+         if(item.modelBrl) {
+             this.milesAndBrl.modelBrl = !this.milesAndBrl.modelBrl
+             this.milesAndBrl.modelMiles = !this.milesAndBrl.modelMiles
+         } else {
+             this.milesAndBrl.modelBrl = !this.milesAndBrl.modelBrl
+             this.milesAndBrl.modelMiles = !this.milesAndBrl.modelMiles
+         }
+     }
+ }
 }
 </script>
 
